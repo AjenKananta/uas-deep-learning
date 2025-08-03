5e2dd50f764f0afbfa03644d4613486556940941
AJENCUYYYY <ajenkananta@gmail.com>
2025-08-03 16:17:57 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model('fer2013_cnn_improved.h5')
+
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        roi = roi_gray.astype('float32') / 255.0
+        roi = np.expand_dims(roi, axis=0)
+        roi = np.expand_dims(roi, axis=-1)
+
+        preds = model.predict(roi, verbose=0)[0]
+        emotion = emotion_labels[np.argmax(preds)]
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+
+    cv2.imshow('Real-time Emotion Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
